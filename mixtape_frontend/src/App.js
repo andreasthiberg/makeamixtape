ef06a478f55c456b8f1bc362cfcd1fe749c07145
Andreas Thiberg <andreas.thiberg@gmail.com>
2023-09-13 17:09:55 +0200

    Cleaned up React app and added new favicon

---
+++ mixtape_frontend/src/App.js
@@ -0,0 +1,13 @@
+import './css/main.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="page-header">
+      Make a mixtape
+      </header>
+    </div>
+  );
+}
+
+export default App;
