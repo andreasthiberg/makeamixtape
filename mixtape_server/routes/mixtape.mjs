2c48533f80051329abd46cf7f3153126e0019ec7
Andreas Thiberg <andreas.thiberg@gmail.com>
2023-09-13 19:46:56 +0200

    Fixed incorrect Atlas connection

--- mixtape_server/routes/mixtape.mjs
+++ mixtape_server/routes/mixtape.mjs
@@ -4,9 +4,9 @@ import db from "../db/connect.mjs"
 
 const router = express.Router();
 
-// This section will help you get a list of all the records.
+// Get all mixtape data.
 router.get("/", async (req, res) => {
-  let collection = await db.collection("records");
+  let collection = await db.collection("mixtapes");
   let results = await collection.find({}).toArray();
   res.send(results).status(200);
 });
--- mixtape_server/server.mjs
+++ mixtape_server/server.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
-import records from "./routes/record.mjs"
+import mixtape from "./routes/mixtape.mjs"
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/record", records);
+app.use("/mixtape", mixtape);
 
 // Start the Express server.
 app.listen(PORT, () => {
